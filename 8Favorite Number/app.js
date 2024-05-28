1a95e5716b429179c9ab648e22a01c6772238281
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 19:09:48 +0500

    Add files via upload

---
+++ 8Favorite Number/app.js
@@ -0,0 +1,3 @@
+var favNumber = 7;
+var myMessage = "My favorite number is \"".concat(favNumber, "\"");
+console.log(myMessage);
---
+++ 8Favorite Number/app.ts
@@ -0,0 +1,5 @@
+let favNumber : number = 7 ;
+
+let myMessage : string = `My favorite number is "${favNumber}"` ;
+
+console.log(myMessage);
