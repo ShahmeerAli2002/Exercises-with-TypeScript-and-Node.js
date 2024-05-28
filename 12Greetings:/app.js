84fa2864b7e2a4779e1618bffcfd01b84670c211
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 19:14:38 +0500

    Add files via upload

---
+++ 12Greetings:/app.js
@@ -0,0 +1,2 @@
+var friendName = ['Taha', "Anas", "Shahroz ", "Ali"];
+friendName.forEach(function (element) { console.log("Hello ".concat(element, ", How are you")); });
---
+++ 12Greetings:/app.ts
@@ -0,0 +1,2 @@
+let friendName : string[] = ['Taha', "Anas" , "Shahroz ", "Ali"]
+ friendName.forEach(element => { console.log(`Hello ${element}, How are you`); });
