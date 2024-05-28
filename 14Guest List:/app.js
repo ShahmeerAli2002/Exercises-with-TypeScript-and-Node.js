333dbdcc83ec11a6d98137ce34de6e56202a1f41
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 19:24:44 +0500

    Add files via upload

---
+++ 14Guest List:/app.js
@@ -0,0 +1,2 @@
+var guestList = ['Taha', "Anas", "Shahroz ", "Ali"];
+guestList.forEach(function (element) { console.log("Salam ".concat(element, ", would you like to dinner with me? ")); });
---
+++ 14Guest List:/app.ts
@@ -0,0 +1,3 @@
+let guestList: string[] = ['Taha', "Anas" , "Shahroz ", "Ali"]
+
+ guestList.forEach(element => { console.log(`Salam ${element}, would you like to dinner with me? `); });
