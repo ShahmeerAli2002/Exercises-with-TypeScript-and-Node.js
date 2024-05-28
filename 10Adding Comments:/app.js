ffa13f9b73f84b906b2e784fea2374c5b73d816a
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 19:11:18 +0500

    Add files via upload

---
+++ 10Adding Comments:/app.js
@@ -0,0 +1,8 @@
+// addition
+console.log(7 + 9);
+// subtraction
+console.log(7 - 6);
+// multiplication
+console.log(7 * 5);
+// division
+console.log(100 / 5);
---
+++ 10Adding Comments:/app.ts
@@ -0,0 +1,15 @@
+// addition
+
+console.log(7+9)
+
+// subtraction
+
+console.log(7 -6)
+
+// multiplication
+
+console.log(7 * 5 )
+
+// division
+
+console.log( 100/ 5 )
