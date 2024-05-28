7edff3ef378c96c9f3608ea4ce6b48fdb4bbde42
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 20:18:15 +0500

    Add files via upload

---
+++ 35Animals/app.js
@@ -0,0 +1,13 @@
+var animals = ['dog', 'cat', 'rabbit'];
+// Printing the name of each animal
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log(animal);
+}
+// Printing a statement about each animal
+for (var _a = 0, animals_2 = animals; _a < animals_2.length; _a++) {
+    var animal = animals_2[_a];
+    console.log("A ".concat(animal, " would make a great pet."));
+}
+// Statement about what these animals have in common
+console.log("Any of these animals would make a great pet!");
---
+++ 35Animals/app.ts
@@ -0,0 +1,14 @@
+let animals: string[] = ['dog', 'cat', 'rabbit'];
+
+// Printing the name of each animal
+for (let animal of animals) {
+    console.log(animal);
+}
+
+// Printing a statement about each animal
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+
+// Statement about what these animals have in common
+console.log("Any of these animals would make a great pet!");
