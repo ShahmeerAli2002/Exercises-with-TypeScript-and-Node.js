976159c940f9bae1d64a495385020b57856167f9
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 20:19:38 +0500

    Add files via upload

---
+++ 36T-Shirt/app.js
@@ -0,0 +1,7 @@
+function make_shirt(size, message) {
+    console.log("The shirt size is ".concat(size, " and it has the message: \"").concat(message, "\" printed on it."));
+}
+// Call the function
+make_shirt('L', 'Hello World');
+make_shirt('M', 'TypeScript is awesome');
+make_shirt('S', 'I love coding');
---
+++ 36T-Shirt/app.ts
@@ -0,0 +1,8 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size} and it has the message: "${message}" printed on it.`);
+}
+
+// Call the function
+make_shirt('L', 'Hello World');
+make_shirt('M', 'TypeScript is awesome');
+make_shirt('S', 'I love coding');
