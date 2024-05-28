88d25b437fcdb33722f6c7b14576738aa45378cf
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 19:04:35 +0500

    Add files via upload

---
+++ 6Stripping Names/app.js
@@ -0,0 +1,4 @@
+var whiteSpaceName = "\n\t SHAMEER ALI \t\n";
+console.log(whiteSpaceName);
+var trimmedName = whiteSpaceName.trim();
+console.log(trimmedName);
---
+++ 6Stripping Names/app.ts
@@ -0,0 +1,6 @@
+let whiteSpaceName : string = "\n\t SHAMEER ALI \t\n"
+ console.log (whiteSpaceName)
+
+let trimmedName = whiteSpaceName.trim()
+
+console.log(trimmedName)
