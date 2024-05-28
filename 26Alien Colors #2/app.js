092b21d8a23d8c8dc117e4eecddba74b025c4e63
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 19:48:08 +0500

    Add files via upload

---
+++ 26Alien Colors #2/app.js
@@ -0,0 +1,16 @@
+var alienColor = 'green';
+// If-else chain to determine points earned
+if (alienColor === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+var alien_color = 'red';
+// If-else chain to determine points earned
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
---
+++ 26Alien Colors #2/app.ts
@@ -0,0 +1,16 @@
+let alienColor: string = 'green';
+
+// If-else chain to determine points earned
+if (alienColor === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+let alien_color: string = 'red';
+
+// If-else chain to determine points earned
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
