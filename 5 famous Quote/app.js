24a8d32840a5272fe24b2df0f329c3bf84919ce2
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-05-28 17:04:50 +0500

    Add files via upload

---
+++ 5 famous Quote/app.js
@@ -0,0 +1,3 @@
+var famous = ("Albert Einstein once said");
+var famousquote = "".concat(famous, ", \"A person who never made a mistake never tried anything new.\"");
+console.log(famousquote);
---
+++ 5 famous Quote/app.ts
@@ -0,0 +1,5 @@
+let famous : string = ("Albert Einstein once said");
+ 
+let famousquote :string = `${famous}, "A person who never made a mistake never tried anything new."`
+
+console.log(famousquote)
